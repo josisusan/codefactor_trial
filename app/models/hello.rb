c00f2871a47ad22c29981672c306c4f59b8a8b82
josisusan <josisusan@gmail.com>
2016-09-29 10:08:16 +0545

    test

--- app/models/hello.rb
+++ app/models/hello.rb
@@ -2,4 +2,19 @@ class Hello < ActiveRecord::Base
   def get_help
     "Help !!!!"
   end
+
+  def abc_complexity
+    here = "asdfasdf"
+    if here
+      there = "adsfadsfasfasdf"
+      if there
+        "asdfasdfasdf"
+      else
+        "asdfasdfadsfadsffasdfsadfdsadfadsfdslllll"
+      end
+      c = "asdfasdfasf"
+    else
+      c = "asdfasdfasf"
+    end
+  end
 end
